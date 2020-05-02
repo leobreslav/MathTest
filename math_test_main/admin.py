3daffb31b82b1701d31798a025d38c4f4ee8f1e8
Matvey <matvik.gurov@gmail.com>
2020-05-02 15:57:43 +0300

    registered models in admin.py

--- math_test_main/admin.py
+++ math_test_main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Set)
+admin.site.register(Task)
+admin.site.register(TestTemplate)
+admin.site.register(Set2Test)
+admin.site.register(TestItem)
+admin.site.register(Profile)
+admin.site.register(TaskItem)
