e898b52a32d53c96923fa5761886e90965ac7a6f
Matvey <matvik.gurov@gmail.com>
2020-05-02 15:47:51 +0300

    renamed all references to MatchTest in manage.py and in MathTest folder

--- MathTest/urls.py
+++ MathTest/urls.py
@@ -1,4 +1,4 @@
-"""MatchTest URL Configuration
+"""MathTest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MatchTest.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MathTest.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
