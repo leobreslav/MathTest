e22292b13813d276060b2ed80659ad3d0145a504
Matvey <matvik.gurov@gmail.com>
2020-05-02 16:47:47 +0300

    added db.py for db configuration

--- MathTest/settings.py
+++ MathTest/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+import json
 import os
+from . import db
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,16 +75,7 @@ WSGI_APPLICATION = 'MathTest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+DATABASES = db.CONFIG
 
 
 # Password validation
