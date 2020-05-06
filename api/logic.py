9ca53313e184d34ac42059942157637e1f000945
hedgeho <ilya.zalessky@yandex.ru>
2020-05-06 15:12:49 +0300

    refactored models.py

--- api/logic.py
+++ api/logic.py
@@ -1,5 +1,6 @@
-from .models import TestTemplate, Prototype2Test
+from .models import TestTemplate, Prototype2Test, TestItem
 from .exceptions import NotAllowedException
+
 
 def generate_test_template(author, name, *task_prototypes):
     if not author.has_access:
@@ -8,7 +9,4 @@ def generate_test_template(author, name, *task_prototypes):
     template = TestTemplate.objects.create(author=author, name=name)
 
     for count, prototype in enumerate(task_prototypes):
-        Prototype2Test.objects.create(test_id=template, set_id=prototype, index=count)
-    
-
-
+        Prototype2Test.objects.create(test=template, set=prototype, index=count)
