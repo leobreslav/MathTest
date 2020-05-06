ba444cb7b4066475203ac0d9b9c71ea728227d05
hedgeho <ilya.zalessky@yandex.ru>
2020-05-06 18:15:39 +0300

    test item generation

--- api/logic.py
+++ api/logic.py
@@ -15,11 +15,16 @@ def generate_test_template(author, name, *task_prototypes):
 
 
 def generate_test_item(template, student):
-    TestItem.objects.create(template=template, student=student)
+    test_item = TestItem.objects.create(template=template, student=student)
 
     prototypes = Prototype2Test.objects.filter(test=template).order_by('index')
 
-    for prototype in prototypes:
+    for i, prototype in enumerate(prototypes):
         problem_heads = ProblemHead.objects.all(prototype=prototype)
-        problem_head = problem_heads[random.range(0, len(problem_heads), 1)]
+        problem_head = problem_heads[random.randrange(0, len(problem_heads), 1)]
 
+        head_item = ProblemHeadItem.objects.create(test=test_item, problem_head=problem_head, index=i)
+
+        points = ProblemPoint.objects.filter(problem_head=problem_head)
+        for j in range(points):
+            ProblemPointItem.objects.create(problem_item=head_item, num_in_problem=j)
