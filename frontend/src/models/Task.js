009ab710c76b2a9c05d32a097259d2cdd82d6c1e
Samathpyth <kriss.sacha@gmail.com>
2020-05-08 12:49:48 +0300

    added frontend source

---
+++ frontend/src/models/Task.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+class Task extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            id: props.id,
+            isLoading: false,
+        }
+    }
+
+    componentDidMount() {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', `/api/problem_heads/${this.state.id}`, true);
+        xhr.send();
+        this.setState({isLoading: true});
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== 4) {
+                return false
+            }
+
+            if (xhr.status !== 200) {
+                console.log(xhr.status + ': ' + xhr.statusText)
+            } else {
+                console.log(xhr);
+                this.setState({
+                    data: JSON.parse(xhr.response),
+                    isLoading: false,
+                })
+            }
+        }
+    }
+
+    renderProducts() {
+        const {data, isLoading} = this.state;
+        if (isLoading) {
+            return <div> Загрузка!!!!</div>
+        } else {
+            return data.map(item => {
+                return <li key={item} > {(item.problem.length > 20? item.problem.substring(0, 19) + "..." : item.problem)}</li>
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div className='App'>
+                <div className='product-list'>
+                    {this.renderProducts()}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Task;
