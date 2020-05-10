91e6d9aae370f278b4d5fb355451bad147e2b3b8
artolord <artgoi111@gmail.com>
2020-05-10 22:19:25 +0300

    make deploy.sh be silent and add logging

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+echo "Starting deploing"
+
 BIN=/srv/math_test/env/bin
 
 set -e
@@ -11,18 +14,20 @@ git reset -q --hard FETCH_HEAD
 
 version=`git rev-parse --short HEAD`
 echo $version > /srv/math_test/static/current_version.txt
-mkdir /srv/math_test/static/$version/
+mkdir -p /srv/math_test/static/$version/
 
-$BIN/pip install -r requirements.txt
+$BIN/pip -qq install -r requirements.txt
 
-$BIN/python manage.py migrate
-$BIN/python manage.py collectstatic
+$BIN/python manage.py migrate --verbosity 0
+$BIN/python manage.py collectstatic --verbosity 0
 
 cd /srv/math_test/app/frontend
 
-/usr/bin/npm install
-/usr/bin/npm run build
+/usr/bin/npm install > /dev/null --quiet --no-progress
+/usr/bin/npm run build > /dev/null
 cp -fr /srv/math_test/app/frontend/build /srv/math_test/react
 
 sudo /bin/systemctl restart uwsgi
 sudo /bin/systemctl restart nginx
+
+echo "Deploing of commit $version in branch $BRANCH is complite"
