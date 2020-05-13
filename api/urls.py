7ee025fc729805ab547ce6dab30b78d4cdcf522c
hedgeho <ilya.zalessky@yandex.ru>
2020-05-13 17:04:18 +0300

    added GET request for templates

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from api.views import ProblemPrototypes, problem_heads, users
+from api.views import ProblemPrototypes, problem_heads, users, test_templates
 
 urlpatterns = [
     path('problem_prototypes', ProblemPrototypes.as_view()),
     path('problem_heads/<int:id>', problem_heads),
     path('problem_heads', problem_heads),
-    path('users', users)
+    path('users', users),
+    path('templates', test_templates)
 ]
