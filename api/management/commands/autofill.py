9dac18a593ac38ae63f211e496aa9c8bd25aaf02
artolord <artgoi111@gmail.com>
2020-05-18 19:55:22 +0300

    fix autofill

--- api/management/commands/autofill.py
+++ api/management/commands/autofill.py
@@ -4,51 +4,39 @@ from api.models import ProblemPrototype, ProblemHead, ProblemPoint, Profile, Pro
 from django.contrib.auth.models import User
 import random
 
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        # User & Profile
-        for i in range(1,6):
-            UserNew = User.objects.create_user('t_username_'+str(i), 't_username_'+str(i)+'@thebeatles.com', 'ja9dsf03DFAd')
-            ProfileNew = Profile.objects.create(user = UserNew, has_access = True)
-            # TestTemplate
-            for j in range(1,3):
-                TestTemplateNew = TestTemplate.objects.create(author = ProfileNew, name = 'Шаблон теста № ' + str(j))
-            # TestTemplate
+from api.logic import generate_test_template, generate_test_item
 
-        # ProblemPrototype
-        for i in range(1,6):
-            ProblemPrototypeNew = ProblemPrototype.objects.create(name = 'Прототип задачи № ' + str(i))
-            # ProblemHead
-            for j in range(1,4):
-                ProblemHeadNew = ProblemHead.objects.create(problem = 'Пример текста головы задачи № ' + str(j))
-                ProblemHeadNew.prototype.add(ProblemPrototypeNew)
-                #ProblemHeadNew.prototype.add(random.choice(ProblemPrototype.objects.all()))
-                ProblemHeadNew.save()
-                # ProblemPoint
-                for k in range(1,3):
-                    ProblemPointNew = ProblemPoint.objects.create(problem_head = ProblemHeadNew,
-                    answer = 'Ответ к задаче № ' + str(j) + ", пункту № " + str(k), num_in_problem = k)
-        # Prototype2Test
-        for each in TestTemplate.objects.all():
-            for i in range(1,4):
-                Prototype2TestNew = Prototype2Test.objects.create(test = each, set = random.choice(ProblemPrototype.objects.all()), index = i)
-        # students & TestItem
-        for i in range(1,11):
-            UserNew = User.objects.create_user('s_username_'+str(i), 's_username_'+str(i)+'@thebeatles.com', 'ja9dsf03DFAd')
-            ProfileNew = Profile.objects.create(user = UserNew, has_access = False)
-            for j in range(1,2):
-                TestItemNew = TestItem.objects.create(student = ProfileNew,
-                                template = random.choice(TestTemplate.objects.all()))
-                for t2t in Prototype2Test.objects.filter(test=TestItemNew.template):
-                    done = False
-                    try:
-                        ProblemHeadItemNew = ProblemHeadItem.objects.create(test = TestItemNew,
-                            problem_head = random.choice(ProblemHead.objects.filter(prototype = t2t.set)),
-                            index = t2t.index)
-                        done = True
-                    except:
-                        done = True
-                    if done:
-                        for pp in ProblemPoint.objects.filter(problem_head = ProblemHeadItemNew.problem_head):
-                            ProblemPointItemNew = ProblemPointItem.objects.create(problem_item = ProblemHeadItemNew, answer = 'Некоторый ответ',
-                            score = random.choice(range(1,4)), comment = 'Некоторый комментарий', num_in_problem = pp.num_in_problem)
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        users = [User.objects.get_or_create(id=i, username=f't_username_{i}', password='ja9dsf03DFAd')[0] for i in range(6)]
+        profiles = [Profile.objects.get_or_create(user=user, has_access=True)[0] for user in users]
+        prototypes = [ProblemPrototype.objects.create(
+            name=f"prototype #{i}",
+            description=f"some description in prototype #{i}"
+            ) for i in range(6)]
+        heads = [
+            ProblemHead.objects.create(
+                problem=f"problem text for problem #{i}"
+            )
+            for i in range(len(prototypes)*10)
+        ]
+        for head in heads:
+            curr_prototypes = random.choices(prototypes, k=random.randrange(1, 3))
+            head.prototype.add(*curr_prototypes)
+
+        points = [
+            ProblemPoint.objects.create(
+                problem_head = head,
+                answer = f"some answer for point {j} in problem {i}",
+                num_in_problem = j
+            )
+            for j in range(random.randrange(1, 5)) for i, head in enumerate(heads)
+        ]
+        for i in range(4):
+            generate_test_template(profiles[i], f"template #{i}", *random.choices(prototypes, k=3))
+
+        templates = [TestTemplate.objects.get(id=i) for i in range(1, 5)]
+        test_items = [
+            generate_test_item(random.choice(templates), random.choice(profiles)) for i in range(20)
+        ]
