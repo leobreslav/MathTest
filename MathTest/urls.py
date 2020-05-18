15433974deee58393a82f0fc2b0d154568880201
artolord <artgoi111@gmail.com>
2020-05-18 17:21:36 +0300

    delete ser

--- MathTest/urls.py
+++ MathTest/urls.py
@@ -3,13 +3,11 @@ from django.urls import re_path, include, path
 
 from MathTest import settings
 from MathTest import views
-from ser.views import autofill
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/', include('api.urls')),
     path('api/admin/', admin.site.urls),
-    path('api/autofill/', autofill),
     path('api/auth/', include('rest_auth.urls')),
     path('api/auth/registration/', include('rest_auth.registration.urls')),
     re_path(r'^', views.index),
